dbda7c79c694b878a1b626b99465b8009265a8a9
Geefy <42178995+Geefy@users.noreply.github.com>
2020-03-09 13:13:53 +0100

    Split it into classes

---
+++ Dekryptering/Encryption101.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dekryptering
+{
+    class Encryption101
+    {
+        static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        Queue<char> swapAlphabet = new Queue<char>(alphabet);
+        char[,] vigSquare = new char[26, 26];
+        public Encryption101()
+        {
+            MakeVigSquare();
+        }
+
+        void MakeVigSquare()
+        {
+            for (int i = 0; i < vigSquare.GetLength(0); i++)
+            {
+                for (int j = 0; j < vigSquare.GetLength(1); j++)
+                {
+                    char temp = swapAlphabet.ElementAt(j);
+                    vigSquare[i, j] = temp;
+                }
+                swapAlphabet.Enqueue(alphabet[i]);
+                swapAlphabet.Dequeue();
+            }
+        }
+
+        public string Encrypt(string text, string pass)
+        {
+            char[] textArr = text.ToCharArray();
+            char[] passArr = pass.ToCharArray();
+            int passIndexer = 0;
+            string encryptedText = "";
+            for (int i = 0; i < textArr.Length; i++)
+            {
+
+                if (passIndexer >= passArr.Length)
+                {
+                    passIndexer = 0;
+                }
+                char textChar = textArr[i];
+                char passChar = passArr[passIndexer];
+                int passIndex = (int)passChar % 32;
+                int textIndex = (int)textChar % 32;
+                encryptedText += vigSquare[passIndex - 1, textIndex - 1];
+                passIndexer++;
+
+            }
+
+            return encryptedText;
+        }
+
+        public string Decrypt(string encryptedText, string pass)
+        {
+            char[] textArr = encryptedText.ToCharArray();
+            char[] passArr = pass.ToCharArray();
+            int passIndexer = 0;
+            string decryptedText = "";
+            for (int i = 0; i < textArr.Length; i++)
+            {
+
+                if (passIndexer >= passArr.Length)
+                {
+                    passIndexer = 0;
+                }
+                char textChar = textArr[i];
+                char passChar = passArr[passIndexer];
+                int passIndex = (int)passChar % 32;
+                int textIndex = (int)textChar % 32;
+                if (textIndex - passIndex < 0)
+                {
+
+                    decryptedText += alphabet[(textIndex + 26) - passIndex];
+                }
+                else
+                {
+
+                    decryptedText += alphabet[textIndex - passIndex];
+                }
+
+                passIndexer++;
+
+            }
+
+            return decryptedText;
+        }
+    }
+}
--- Dekryptering/Program.cs
+++ Dekryptering/Program.cs
@@ -3,99 +3,41 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Dekryptering
 {
     class Program
     {
-        static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-        static Queue<char> swapAlphabet = new Queue<char>(alphabet);
-        static char[,] vigSquare = new char[26, 26];
-        static string key = "Secretkey";
-        static string encryptionText = "HelloMikkel";
 
         static void Main(string[] args)
         {
-            MakeVigSquare();
-            Console.WriteLine("Text to encrypt: " + encryptionText +" with key: " + key);
-            Console.WriteLine("Text encrypted: " + Encrypt(encryptionText, key));
-            Console.WriteLine("Text decrypted: " + Decrypt(Encrypt(encryptionText, key), key));
-            Console.Read();
-
-
-        }
-        static void MakeVigSquare()
-        {
-            for (int i = 0; i < vigSquare.GetLength(0); i++)
-            {
-                for (int j = 0; j < vigSquare.GetLength(1); j++)
-                {
-                    char temp = swapAlphabet.ElementAt(j);
-                    vigSquare[i, j] = temp;
-                }
-                swapAlphabet.Enqueue(alphabet[i]);
-                swapAlphabet.Dequeue();
-            }
-        }
-
-        static string Encrypt(string text, string pass)
-        {
-            char[] textArr = text.ToCharArray();
-            char[] passArr = pass.ToCharArray();
-            int passIndexer = 0;
-            string encryptedText = "";
-            for (int i = 0; i < textArr.Length; i++)
+            while (true)
             {
 
-                if (passIndexer >= passArr.Length)
-                {
-                    passIndexer = 0;
-                }
-                char textChar = textArr[i];
-                char passChar = passArr[passIndexer];
-                int passIndex = (int)passChar % 32;
-                int textIndex = (int)textChar % 32;
-                encryptedText += vigSquare[passIndex - 1, textIndex - 1];
-                passIndexer++;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            string key = "Secretkey";
+            string encryptionText = "HelloMikkel";
 
+            Encryption101 vigEncrypter = new Encryption101();
+
+
+            string temp = vigEncrypter.Encrypt(encryptionText, key);
+            Console.WriteLine(temp);
+
+
+            string reTemp = vigEncrypter.Decrypt(temp, key);
+            Console.WriteLine(reTemp);
+
+                watch.Stop();
+                Console.WriteLine(watch.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
             }
 
-            return encryptedText;
+
         }
 
-        static string Decrypt(string encryptedText, string pass)
-        {
-            char[] textArr = encryptedText.ToCharArray();
-            char[] passArr = pass.ToCharArray();
-            int passIndexer = 0;
-            string decryptedText = "";
-            for (int i = 0; i < textArr.Length; i++)
-            {
 
-                if (passIndexer >= passArr.Length)
-                {
-                    passIndexer = 0;
-                }
-                char textChar = textArr[i];
-                char passChar = passArr[passIndexer];
-                int passIndex = (int)passChar % 32;
-                int textIndex = (int)textChar % 32;
-                if (textIndex - passIndex < 0)
-                {
-
-                    decryptedText += alphabet[(textIndex + 26) - passIndex];
-                }
-                else
-                {
-
-                    decryptedText += alphabet[textIndex - passIndex];
-                }
-
-                passIndexer++;
-
-            }
-
-            return decryptedText;
-        }
     }
 }
